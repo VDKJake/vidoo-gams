6a842f7016da485a5e172efd1e1769acc2984d17
Casey Clare-Turner <100573942@student.swin.edu.au>
2017-11-19 04:15:41 +1100

    -Added a tag for platforms. There isn't anything that uses it yet, but I though it might be useful. -Changed the platform to have 1 collider instead of 1 for each segment of the sprite. -Simplified the one way platform code.

--- FluffoDoggo/Assets/Scripts/OneWayPlatform.cs
+++ FluffoDoggo/Assets/Scripts/OneWayPlatform.cs
@@ -6,12 +6,13 @@ public class OneWayPlatform : MonoBehaviour {
     //NOTES
     //The trigger collider that this script references, needs to start under the platform, and finish before the top of the platform.
 
-    BoxCollider2D[] platform;
+    BoxCollider2D[] colliders;
+    BoxCollider2D platform;
 
 	// Use this for initialization
 	void Start () {
-        platform = GetComponentsInChildren<BoxCollider2D>(); //Grab the colliders. I's like to find a better way to do this, that doesn't grab the trigger collider too
-        //This assumes that this is the parent object of each of the platform sprites
+        colliders = GetComponents<BoxCollider2D>(); //Grab the colliders. I's like to find a better way to do this, that doesn't grab the trigger collider too
+        platform = colliders[0];
 	}
 	
 	// Update is called once per frame
@@ -23,10 +24,9 @@ public class OneWayPlatform : MonoBehaviour {
     {
         if (collision.tag == "Player") //Check that the player has entered the trigger
         {
-            foreach (BoxCollider2D b in platform)
             {
-                if (b.isTrigger == false) Physics2D.IgnoreCollision(collision, b); //If the collider is not the trigger collider, this collider will ignore collisions with the player
-                //Debug.Log("Collision OFF: " + b.gameObject.ToString());
+                Physics2D.IgnoreCollision(collision, platform); //If the collider is not the trigger collider, this collider will ignore collisions with the player
+                //Debug.Log("Collision OFF");
             }
         }
     }
@@ -35,11 +35,8 @@ public class OneWayPlatform : MonoBehaviour {
     {
         if (collision.tag == "Player")
         {
-            foreach (BoxCollider2D b in platform)
-            {
-                if (b.isTrigger == false) Physics2D.IgnoreCollision(collision, b, false); //un-ignoring collisions with the player
-                //Debug.Log("Collision ON: " + b.gameObject.ToString());
-            }
+            Physics2D.IgnoreCollision(collision, platform, false); //un-ignoring collisions with the player
+            //Debug.Log("Collision ON:");
         }
     }
 }
